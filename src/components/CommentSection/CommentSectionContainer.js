b1406db1994431179e19fb803f2668eb48d65f47
Dylan Collins <dcollins5590@students.mc3.edu>
2020-02-18 15:31:22 -0500

    finished the mvp for the project

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,11 +4,12 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
-const CommentSection = props => {
+const CommentSection = ({comments}) => {
+  const [comment] = useState(comments)
   // Add state for the comments
-
   return (
     <div>
+      {comment.map((item, index) => (<Comment key={index} username={item.username} text={item.text}/> ))}
       {/* map through the comments data and return the Comment component */}
       <CommentInput />
     </div>
